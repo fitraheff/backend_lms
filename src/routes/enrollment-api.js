8c000e9c78a10eb78d9f23e589c6ad2bbab67e25
fitraheff <mhmdfitrah24@gmail.com>
2026-02-16 21:55:27 +0700

    enrollment route done

---
+++ src/routes/enrollment-api.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import enrollmentControll from '../controllers/enrollment-controll';
+import auth from '../middlewares/auth-middlaware.js';
+
+const enrollmentRoute = express.Router();
+
+enrollmentRoute.post('/:classId', auth.authMiddleware, enrollmentControll.enroll);
+enrollmentRoute.get('/me', auth.authMiddleware, auth.restrictTo('STUDENT'), enrollmentControll.getMyEnrollment);
+enrollmentRoute.delete('/:classId', auth.authMiddleware, auth.restrictTo('STUDENT'), enrollmentControll.cancelEnrollment);
+
+export default enrollmentRoute
