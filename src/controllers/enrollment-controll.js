f7452b2ada9f5266b045ce998c35be1a1c245dd3
fitraheff <mhmdfitrah24@gmail.com>
2026-02-16 21:49:47 +0700

    enrollment service done

---
+++ src/controllers/enrollment-controll.js
@@ -0,0 +1,44 @@
+import enrollmentService from "../services/enrollment-service";
+
+const enroll = async (req, res, next) => {
+    try {
+        const userId = req.user.id; 
+        const classId = req.params.classId;
+        const result = await enrollmentService.enroll(userId, classId);
+        res.status(200).json({
+            message: "Enroll class successful",
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+const getMyEnrollment = async (req, res, next) => {
+    try {
+        const userId = req.user.id; 
+        const result = await enrollmentService.getMyEnrollment(userId); 
+        res.status(200).json({
+            message: "Get my enrollment successful",
+            data: result
+        });     
+    } catch (e) {
+        next(e);
+    }
+}
+
+const cancelEnrollment = async (req, res, next) => {
+    try {
+        const userId = req.user.id; 
+        const classId = req.params.classId;
+        const result = await enrollmentService.cancelEnrollment(userId, classId);
+        res.status(200).json({
+            message: "Cancel enrollment successful",
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export default { enroll, getMyEnrollment, cancelEnrollment };
